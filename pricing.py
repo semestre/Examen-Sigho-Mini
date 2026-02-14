cd6989fcc0071074e8c68cc2301a27c7f4799296
SIGHO Legacy Team <docente@curso.edu>
2026-02-14 11:00:00 +0000

    Agregar categoría ropa (mismo esquema de descuento que electrónica)

--- pricing.py
+++ pricing.py
@@ -10,8 +10,22 @@ def calcular_precio_electronica(precio, cantidad):
     return subtotal - descuento + impuesto
 
 
+def calcular_precio_ropa(precio, cantidad):
+    subtotal = precio * cantidad
+    if cantidad > 10:
+        descuento = subtotal * 0.15
+    elif cantidad > 5:
+        descuento = subtotal * 0.10
+    else:
+        descuento = 0
+    impuesto = (subtotal - descuento) * 0.16
+    return subtotal - descuento + impuesto
+
+
 def calcular_precio_final(precio, cantidad, categoria):
     if categoria == "electronica":
         return calcular_precio_electronica(precio, cantidad)
+    elif categoria == "ropa":
+        return calcular_precio_ropa(precio, cantidad)
     else:
         return precio * cantidad
