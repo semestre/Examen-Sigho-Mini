1776e6cf80675173776ecbe6153ae4a796414e01
SIGHO Legacy Team <docente@curso.edu>
2026-03-20 10:20:00 +0000

    Agregar categoría alimentos con su propio impuesto reducido

--- pricing.py
+++ pricing.py
@@ -22,10 +22,22 @@ def calcular_precio_ropa(precio, cantidad):
     return subtotal - descuento + impuesto
 
 
+def calcular_precio_alimentos(precio, cantidad):
+    subtotal = precio * cantidad
+    if cantidad > 20:
+        descuento = subtotal * 0.05
+    else:
+        descuento = 0
+    impuesto = (subtotal - descuento) * 0.08
+    return subtotal - descuento + impuesto
+
+
 def calcular_precio_final(precio, cantidad, categoria):
     if categoria == "electronica":
         return calcular_precio_electronica(precio, cantidad)
     elif categoria == "ropa":
         return calcular_precio_ropa(precio, cantidad)
+    elif categoria == "alimentos":
+        return calcular_precio_alimentos(precio, cantidad)
     else:
         return precio * cantidad
